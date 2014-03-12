7b8afe553e69dfe85c98da2d62263885c72b70d8
Dan Goldin <dangoldin@gmail.com>
2014-03-11 20:53:24 -0400

    Added to csv conversion

--- get_books.py
+++ get_books.py
@@ -1,7 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import requests
 import json
 import os
 import time
+import csv
 
 def get_sets():
     r = requests.get('https://www.oysterbooks.com/api/sets?limit=1000')
@@ -22,14 +25,40 @@ def get_books(set_info):
         f.write(r.content)
     return j
 
-book_sets = get_sets()
-all_books = []
-for book_set in book_sets:
-    books = get_books(book_set)
-    all_books.extend( books )
+def flatten_dict(d, s =''):
+    new = {}
+    for k, v in d.items():
+        key = s + k
+        if isinstance(v, dict):
+            new.update(flatten_dict(v, key + '_'))
+        else:
+            if isinstance(v, basestring):
+                new[key] = v.encode('utf-8').strip()
+            else:
+                new[key] = v
+    return new
 
-with open( os.path.join('data', 'all-books.json'), 'w') as f:
-    f.write( json.dumps(all_books, indent=2) )
+def to_csv(json_file):
+    with open(json_file, 'r') as f:
+        j = json.loads(f.read())
+        jf = [flatten_dict(x) for x in j]
+        with open( json_file.replace('.json', '.csv'), 'w') as g:
+            writer = csv.DictWriter(g, fieldnames=jf[0].keys())
+            writer.writeheader()
+            writer.writerows(jf)
 
-for book in all_books:
-    print book['book']['title'], book['book']['author'], book['book']['status'], book['book']['isbn']
+# book_sets = get_sets()
+# all_books = []
+# for book_set in book_sets:
+#     books = get_books(book_set)
+#     all_books.extend( books )
+
+# with open( os.path.join('data', 'all-books.json'), 'w') as f:
+#     f.write( json.dumps(all_books, indent=2) )
+
+# for book in all_books:
+#     print book['book']['title'], book['book']['author'], book['book']['status'], book['book']['isbn']
+
+to_csv( os.path.join('data', 'all-books.json') )
+
+#d <- csv.read('/Users/danielgoldin/Dropbox/dev/web/oyster-books-crawl/data/all-books.csv', header=TRIE,
